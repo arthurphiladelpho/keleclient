2934b08acd6e4fd5989537a17117cf7818a2cf40
Arthur Philadelpho <arthur22rj@gmail.com>
2017-06-07 20:30:06 -0300

    First try: roadmap is returned, get_checkpoint returns 'sorry, not authorized' message.

--- lib/kele.rb
+++ lib/kele.rb
@@ -26,4 +26,18 @@ class Kele
 		end	
 	end
 
+	def get_roadmap(roadmap_id)
+    response = self.class.get("/roadmaps/#{roadmap_id}", headers: { "authorization" => @auth_token })
+    response = JSON.parse(response.body)
+    @roadmap = response["name"]
+    puts "Your roadmaps is #{@roadmap}."
+    return @roadmap
+    # checkpoint id is response["sections"]["checkpoints"]["id" == 1901]
+  end
+
+  def get_checkpoint(checkpoint_id)
+    response = self.class.get("/checkpoints/#{checkpoint_id}", headers: { "authorization" => @auth_token })
+    response = JSON.parse(response.body)
+  end
+
 end
