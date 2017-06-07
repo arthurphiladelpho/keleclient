340a98244239d6dc262a74ef23ffdc610f8800ae
Arthur Philadelpho <arthur22rj@gmail.com>
2017-06-07 18:26:46 -0300

    Checkpoint 3: Retrieve Users - Complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,8 +1,9 @@
 require "httparty"
+require "json"
 
 class Kele
 	include HTTParty
-
+	include JSON
 	base_uri 'https://www.bloc.io/api/v1'
 	
 	def initialize(email, password)
@@ -12,6 +13,17 @@ class Kele
 		raise "Invalid email or password" if response.code == 404
 		@auth_token = response["auth_token"]
 	
+	end
+
+	def get_me
+		response = self.class.get("/users/me", headers: { "authorization" => @auth_token } )
+		@current_user = JSON.parse(response.body)
+		@current_user.each do |key, value|
+		  if key == "name"
+		  	puts "Current user is #{value}."
+		  	return value
+		  end
+		end	
 	end
 
 end
