135723182d730feb15f3e08af90b5faf1329f904
Arthur Philadelpho <arthur22rj@gmail.com>
2017-06-12 18:35:12 -0300

    Passed get_roadmap and get_checkpoint methods to a Roadmap module.

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,9 +1,12 @@
 require "httparty"
 require "json"
+require "roadmap"
 
 class Kele
 	include HTTParty
 	include JSON
+	include Roadmap
+
 	base_uri 'https://www.bloc.io/api/v1'
 	
 	def initialize(email, password)
@@ -25,20 +28,5 @@ class Kele
 		  end
 		end	
 	end
-
-	def get_roadmap(roadmap_id)
-    response = self.class.get("/roadmaps/#{roadmap_id}", headers: { "authorization" => @auth_token })
-    response = JSON.parse(response.body)
-    @roadmap = response["name"]
-    puts "Your roadmaps is #{@roadmap}."
-    puts response
-    return @roadmap
-    # checkpoint id is response["sections"]["checkpoints"]["id" == 1901]
-  end
-
-  def get_checkpoint(checkpoint_id)
-    response = self.class.get("/checkpoints/#{checkpoint_id}", headers: { "authorization" => @auth_token })
-    response = JSON.parse(response.body)
-  end
 
 end
