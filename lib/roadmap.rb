c55210704c3170c07a8ac6e4b9fcd5f68887d5ad
Arthur Philadelpho <arthur22rj@gmail.com>
2017-06-26 16:42:13 -0300

    Ran Rubocop autocorrect.

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,17 +1,15 @@
 module Roadmap
-
-	def get_roadmap(roadmap_id)
-    response = self.class.get("/roadmaps/#{roadmap_id}", headers: { "authorization" => @auth_token })
+  def get_roadmap(roadmap_id)
+    response = self.class.get("/roadmaps/#{roadmap_id}", headers: { 'authorization' => @auth_token })
     response = JSON.parse(response.body)
-    @roadmap = response["name"]
+    @roadmap = response['name']
     puts "Your roadmaps is #{@roadmap}."
     puts response
-    return @roadmap
+    @roadmap
   end
 
   def get_checkpoint(checkpoint_id)
-    response = self.class.get("/checkpoints/#{checkpoint_id}", headers: { "authorization" => @auth_token })
+    response = self.class.get("/checkpoints/#{checkpoint_id}", headers: { 'authorization' => @auth_token })
     response = JSON.parse(response.body)
   end
-  
 end
